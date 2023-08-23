3ccc13f7c120873fefc4f659482330e353fa1786
valdezae <A00829909@itesm.mx>
2023-08-22 20:28:27 -0600

    Added methods for Packages and Ant

--- bot_cleaners/model.py
+++ bot_cleaners/model.py
@@ -6,6 +6,7 @@ from mesa.datacollection import DataCollector
 
 import numpy as np
 import math
+import random
 
 
 class ChargingStation(Agent):
@@ -22,8 +23,9 @@ class Ant(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.next_position = None
-        self.state = 0  # 0: Rest #1: Moving #2: Hauling #3: Charging
+        self.state = 0  # 0: Rest #1: Picking Up(Conveyor) #2: Picking Up(Shelves) #3: Delivering #4: Charging
         self.haul_sku = None
+        self.haul_destination_pos = None
         self.charge_percentage = 100
         self.target_pos = None
         self.charging_stations = [(5, 5), (5, 15), (15, 5), (15, 15)]
@@ -36,10 +38,6 @@ class Ant(Agent):
             if distance < least_distance_to_target:
                 least_distance_to_target = distance
                 self.next_position = neighbour.pos
-
-    def haul_package(self, package_sku):
-        self.state = 2
-        self.haul_sku = package_sku
 
     def deliver_package(self):
         self.state = 1
@@ -55,7 +53,7 @@ class Ant(Agent):
 
         filtered_neighbours = list()
 
-        if self.state == 2:
+        if self.state == 3:
             for neighbour in neighbours:
                 if isinstance(neighbour, (Cell, ChargingStation, Conveyors)):
                     filtered_neighbours.append(neighbour)
@@ -64,32 +62,75 @@ class Ant(Agent):
                 if isinstance(neighbour, (Cell, ChargingStation, Conveyors, Shelves)):
                     filtered_neighbours.append(neighbour)
 
-        self.move_to_target_pos(filtered_neighbours)
+        for cell in self.model.grid.coord_iter():
+            cell_content, pos = cell
+            for obj in cell_content:
+                # from receiving conveyor to shelves
+                if isinstance(obj, Conveyors) and obj.pos == self.pos and self.state == 1:
+                    self.target_pos = self.haul_destination_pos
+                    self.state = 3
+                # from shelves and to exit conveyor
+                elif isinstance(obj, Shelves) and obj.pos == self.pos and self.state == 2:
+                    self.target_pos = self.haul_destination_pos
+                    self.state = 3
+                # leaving at exit conveyor
+                elif isinstance(obj, Conveyors) and obj.pos == self.pos and self.state == 3:
+                    self.state = 0
+                    self.target_pos = (0, 0)
 
-
+        if self.charge_percentage <= 25:
+            self.state = 4
+            self.target_pos = self.charging_stations[random.randint(0, 3)]
 
         if self.pos in self.charging_stations and self.charge_percentage < 99:
             self.charge(self.charge_percentage)
-            self.next_position = self.pos
+            self.target_pos = self.pos
             return
-        elif
 
-
+        self.move_to_target_pos(filtered_neighbours)
 
 
 class Conveyors(Agent):
-    def __init__(self, unique_id, model, has_package: bool = False):
+    def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.conveyor_id = None
-        self.has_package = has_package
+        self.has_package = False
 
 
 class Packages(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.sku = unique_id
-        self.state = 0 #0: In storage #1: Awaiting pickup #2: In transit #3: Sent
+        self.state = 0  # 0: In storage #1: Awaiting pickup #2: In transit #3: Sent
         self.peso = None
+        self.target_pos = None
+        self.next_position = None
+
+    def move_to_target_pos(self, neighbour_list):
+        least_distance_to_target = math.inf
+        target_position = self.target_pos
+        for neighbour in neighbour_list:
+            distance = math.dist(neighbour.pos, target_position)
+            if distance < least_distance_to_target:
+                least_distance_to_target = distance
+                self.next_position = neighbour.pos
+
+    def step(self):
+        neighbours = self.model.grid.get_neighbors(
+            self.pos, moore=True, include_center=True)
+
+        filtered_neighbours = list()
+
+        for neighbour in neighbours:
+            if isinstance(neighbour, (Ant, Cell, ChargingStation, Conveyors)) and neighbour.haul_sku == self.sku:
+                filtered_neighbours.append(neighbour)
+
+        if self.state == 1:
+            for cell in self.model.grid.coord_iter():
+                cell_content, pos = cell
+                for obj in cell_content:
+                    if isinstance(obj, Ant) and obj.pos == self.pos:
+                        self.target_pos = filtered_neighbours[0].target_pos
+                        self.state = 2
 
 
 class Shelves(Agent):
